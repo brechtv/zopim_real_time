8af05cdf0583ed593e03acd1a70a0cc44abe9579
Brecht Vermeire <brecht@Brechts-MacBook-Pro.local>
2018-09-03 14:37:59 +0100

    historical charts

--- app.js
+++ app.js
@@ -1,6 +1,8 @@
 var department_id;
 var department_name;
 var token;
+var chats_historical = [];
+var online_historical = [];
 
 init()
 
@@ -10,14 +12,15 @@ function init() {
     token = localStorage.getItem('token');
     $("#oauth_token").val(token)
     refresh_agents(token)
-    refresh_chats(token)
-    if(department_id) {
+    if (department_id) {
         refresh_agents_for_department(department_id, department_name, token)
         refresh_departments(token)
     } else {
         refresh_departments(token)
     }
-    setInterval(function(){ refresh() }, 60000);
+    setInterval(function() {
+        refresh()
+    }, 60000);
 
 }
 
@@ -40,27 +43,41 @@ function refresh_agents(token) {
     $("#agents_online").html(`<i class="material-icons md-48">sync</i>`)
     $("#agents_invisible").html(`<i class="material-icons md-48">sync</i>`)
     $.ajax({
-        url: "https://bvcors.herokuapp.com/https://rtm.zopim.com/stream/agents",
-        headers: {"Authorization": "Bearer " + token}
-    }).done(function(result) {
-        $("#authenticate_toggle").text("Authenticated")
-        $("#authenticate_toggle").css("background-color", "#4CAF50")
-        $("#refresh").prop('disabled', false);
-        var data = result.content.data
-        $("#agents_away").text(data.agents_away)
-        $("#agents_online").text(data.agents_online)
-        $("#agents_invisible").text(data.agents_invisible)
-        // $(".agent-ticker").removeClass("blur")
-        var online_pct = (data.agents_online / (data.agents_online + data.agents_away) * 100).toFixed(0) + "%"
-        $("#online_percentage").text(online_pct)
-        $("#online_percentage_bar").css("width", online_pct)
-        refresh_chats(token)
-    })
-    .fail(function(result) {
-        $("#authenticate_toggle").text("Not Authenticated")
-        $("#authenticate_toggle").css("background-color", "#F84066")
-        $("#refresh").prop('disabled', true);
-    })
+            url: "https://bvcors.herokuapp.com/https://rtm.zopim.com/stream/agents",
+            headers: {
+                "Authorization": "Bearer " + token
+            }
+        }).done(function(result) {
+            var date = new Date()
+            $("#authenticate_toggle").text("Authenticated")
+            $("#authenticate_toggle").css("background-color", "#4CAF50")
+            $("#refresh").prop('disabled', false);
+            var data = result.content.data
+            $("#agents_away").text(data.agents_away)
+            $("#agents_online").text(data.agents_online)
+            $("#agents_invisible").text(data.agents_invisible)
+                // $(".agent-ticker").removeClass("blur")
+            var online_pct = (data.agents_online / (data.agents_online + data.agents_away) * 100).toFixed(0) + "%"
+            $("#online_percentage").text(online_pct)
+            $("#online_percentage_bar").css("width", online_pct)
+
+            if (online_historical.length > 60) {
+                online_historical.push([[date.getHours(), date.getMinutes(), date.getSeconds()], data.agents_online])
+            } else {
+                online_historical.shift
+                online_historical.push([[date.getHours(), date.getMinutes(), date.getSeconds()], data.agents_online])
+            }
+            if (online_historical) {
+                draw_historical(online_historical, 'online_historical')
+            }
+
+            refresh_chats(token)
+        })
+        .fail(function(result) {
+            $("#authenticate_toggle").text("Not Authenticated")
+            $("#authenticate_toggle").css("background-color", "#F84066")
+            $("#refresh").prop('disabled', true);
+        })
 }
 
 // chats
@@ -70,23 +87,43 @@ function refresh_chats(token) {
     $("#active_chats").html(`<i class="material-icons md-48">sync</i>`)
     $.ajax({
         url: "https://bvcors.herokuapp.com/https://rtm.zopim.com/stream/chats",
-        headers: {"Authorization": "Bearer " + token}
+        headers: {
+            "Authorization": "Bearer " + token
+        }
     }).done(function(result) {
         var data = result.content.data
+        var date = new Date();
+        if (chats_historical.length > 60) {
+            chats_historical.push([[date.getHours(), date.getMinutes(), date.getSeconds()], data.active_chats])
+        } else {
+            chats_historical.shift
+            chats_historical.push([[date.getHours(), date.getMinutes(), date.getSeconds()], data.active_chats])
+        }
+        if (chats_historical) {
+            draw_historical(chats_historical, 'chats_historical')
+        }
+
         $("#incoming_chats").text(data.incoming_chats)
-    	$("#assigned_chats").text(data.assigned_chats)
-    	$("#active_chats").text(data.active_chats)
+        $("#assigned_chats").text(data.assigned_chats)
+        $("#active_chats").text(data.active_chats)
 
         // calculate capacity
         var agents = $("#agents_online").text()
         var chats = $("#active_chats").text()
-        var calculated_capacity = (chats / (agents * 2) * 100).toFixed(0) + "%"
-        console.log(calculated_capacity)
+        var calculated_capacity = (chats / (agents * 2) * 100)
+        if (calculated_capacity > 80) {
+            $(".active-chats-card").addClass("buzz");
+            setTimeout(function() {
+                $(".active-chats-card").removeClass("buzz");
+            }, 1000);
+        } else {
+            $(".active-chats-card").removeClass("buzz")
+        }
+        calculated_capacity = (chats / (agents * 2) * 100).toFixed(0) + "%"
         $("#calculated_capacity").text(calculated_capacity)
         $("#calculated_capacity_bar").css("width", calculated_capacity)
 
         /// update last refreshed timestamp
-        var date = new Date()
         $("#last_refreshed_value").text(date.toTimeString())
     });
 }
@@ -95,7 +132,9 @@ function refresh_chats(token) {
 function refresh_departments(token) {
     $.ajax({
         url: "https://bvcors.herokuapp.com/https://www.zopim.com/api/v2/departments",
-        headers: {"Authorization": "Bearer " + token}
+        headers: {
+            "Authorization": "Bearer " + token
+        }
     }).done(function(result) {
         $("#department_dropdown_options").empty()
         $.each(result, function(i, v) {
@@ -121,7 +160,9 @@ function refresh_agents_for_department(department_id, department_name, token) {
     $("#current_department").text(department_name)
     $.ajax({
         url: "https://bvcors.herokuapp.com/https://rtm.zopim.com/stream/agents?department_id=" + department_id,
-        headers: {"Authorization": "Bearer " + token}
+        headers: {
+            "Authorization": "Bearer " + token
+        }
     }).done(function(result) {
         var data = result.content.data
         var online_pct = (data.agents_online / (data.agents_online + data.agents_away) * 100).toFixed(0) + "%"
@@ -129,6 +170,31 @@ function refresh_agents_for_department(department_id, department_name, token) {
         $("#department_online_percentage").text(online_pct)
         $("#department_agents_away").text(data.agents_away)
         $("#department_agents_invisible").text(data.agents_invisible)
-        // $(".department-ticker").removeClass("blur")
+            // $(".department-ticker").removeClass("blur")
     });
+}
+
+function draw_historical(chats_array, target_container) {
+    $("#" + target_container).empty()
+    var data = new google.visualization.DataTable();
+    data.addColumn('timeofday', 'Time');
+    if(target_container == "chats_historical") {
+        data.addColumn('number', 'Chats');
+    } else {
+        data.addColumn('number', 'Agents Online');
+    }
+    
+    data.addRows(chats_array)
+    var options = {
+        height: 100,
+        legend: {position: 'none'},
+        hAxis: { title: '' },
+        series: {
+            0: { color: (target_container == "chats_historical" ? "#0092E5" : "#F84066") }
+        }
+      };
+
+      var chart = new google.charts.Line(document.getElementById(target_container));
+
+      chart.draw(data, google.charts.Line.convertOptions(options));
 }
